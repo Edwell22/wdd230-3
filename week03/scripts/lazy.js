e3efc854727f2842a712c049fe8d6bdd1e872477
Edwell22 <edwellkakunguwo358@gmail.com>
2024-01-26 16:21:36 +0200

    new

--- week03/scripts/lazy.js
+++ week03/scripts/lazy.js
@@ -1,3 +1,15 @@
+// const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" };
+// document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+
+const date = new Date();
+
+let year = date.getFullYear();
+
+let currentYear = `${year}`;
+
+document.querySelector("#year").textContent = currentYear;
+
+document.getElementById("currentdate").textContent = document.lastModified;
 const allImages = document.querySelectorAll("img[data-src]")
 
 const lazyLoad = (img) => {
